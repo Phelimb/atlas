f90622e5a029cd3fa28695aab5ca4137002c3e86
Phelim Bradley <wave@phel.im>
2016-11-13 13:12:50 +0000

    Moved vcf to atlasvar

--- mykatlas/treeplacing/__init__.py
+++
@@ -1,4 +0,0 @@
-from mykatlas.treeplacing.models import Placer
-from mykatlas.treeplacing.models import Node
-from mykatlas.treeplacing.models import Leaf
-from mykatlas.treeplacing.newick2json import newick2json
